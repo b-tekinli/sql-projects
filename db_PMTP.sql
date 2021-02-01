46ee70856a0f3a1c410f0b72115e3cf7e26a2e6e
Beyzanur Tekinli <btbtbt664@gmail.com>
2021-02-01 17:22:18 +0300

    Update db_PMTP.sql

--- db_PMTP.sql
+++ db_PMTP.sql
@@ -2,7 +2,7 @@ USE [master]
 GO
 
 
-/****** Veritabanını Oluşturma ******/
+/****** VeritabanÄ± OluÅŸturma ******/
 
 
 CREATE DATABASE db_PMTP
@@ -19,7 +19,7 @@ GO
 
 
 
-/****** Tabloları ve Anahtar Alanları Oluşturma – UNIQUE DEFAULT ve PRİMARY KEYLER TABLOLARIN İÇİNDE BULUNUYOR. ******/
+/****** TablolarÄ± ve Anahtar AlanlarÄ± OluÅŸturma â€“ UNIQUE DEFAULT ve PRIMARY KEYLER TABLOLARIN Ä°Ã‡Ä°NDE BULUNUYOR. ******/
 
 
 CREATE TABLE tbl_Bolumler
@@ -36,7 +36,7 @@ CREATE TABLE tbl_Personeller
 Pers_ID INT not null PRIMARY KEY IDENTITY(1,1),
 Pers_Adi nvarchar(30) not null,
 Pers_Soyadi nvarchar(30) not null,
-[Pers_İsim] AS ([Pers_Adi] + [Pers_Soyadi]),
+[Pers_Ãsim] AS ([Pers_Adi] + [Pers_Soyadi]),
 Pers_DTarihi date null,
 Pers_Ise_Giris_Tarihi date DEFAULT GETDATE(),
 Pers_Isten_Cikis_Tarihi datetime null,
@@ -95,25 +95,25 @@ Maas_Yili date not null,
 /****** Verileri Girme ******/
 
 
-insert into tbl_Bolumler(Bolum_Adi,Bolum_Tel,Mudur_ID) values ('Bilişim Sistemi','111','5')
+insert into tbl_Bolumler(Bolum_Adi,Bolum_Tel,Mudur_ID) values ('BiliÃ¾im Sistemi','111','5')
 insert into tbl_Bolumler(Bolum_Adi,Bolum_Tel,Mudur_ID) values ('Pazarlama','113','1')
-insert into tbl_Bolumler(Bolum_Adi,Bolum_Tel,Mudur_ID) values ('Satış','114','1')
+insert into tbl_Bolumler(Bolum_Adi,Bolum_Tel,Mudur_ID) values ('SatÃ½Ã¾','114','1')
 insert into tbl_Bolumler(Bolum_Adi,Bolum_Tel,Mudur_ID) values ('Muhasebe','115','2')
 insert into tbl_Bolumler(Bolum_Adi,Bolum_Tel,Mudur_ID) values ('Finans','115','4')
 
 
-insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Melike','Şen','07.02.2002','11.11.2020','','4 Temmuz Mah.','Kocaeli','Karamürsel','41','12345678910','6845131831','melike@gmail.com','1','','','0','1','8453121558','45678215469','1','','','','','','')
-insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Ahmet','Tekin','08.10.1970','01.09.2000','','4 Temmuz Mah. 15/2','Kocaeli','Karamürsel','41','01234567899','6845118321','ahmet@gmail.com','2','','','1','2','8618426545','64651351112','1','','','','','','')
-insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Selen','Ak','09.12.2000','05.07.2020','','Kentkonutlar','Kocaeli','Karamürsel','41','45674125745','6845131321','selen@gmail.com','3','','','0','3','7624537532','46846512312','1','','','','','','')
-insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Buse','Yılmaz','12.12.1999','03.12.2020','','Ferah Mah. Çamlıca','İstanbul','Üsküdar','34','78945612332','6851318321','buse@gmail.com','4','','','0','4','7867343455','23453786544','1','','','','','','')
-insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Aleyna','Öz','11.05.2000','04.02.2020','','Dumlupınar Mah. 19/05','Trabzon','Of','61','12312312345','68451318321','aleyna@gmail.com','5','','','0','5','4264146748','45126435778','1','','','','','','')
+insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Melike','Ãen','07.02.2002','11.11.2020','','4 Temmuz Mah.','Kocaeli','KaramÃ¼rsel','41','12345678910','6845131831','melike@gmail.com','1','','','0','1','8453121558','45678215469','1','','','','','','')
+insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Ahmet','Tekin','08.10.1970','01.09.2000','','4 Temmuz Mah. 15/2','Kocaeli','KaramÃ¼rsel','41','01234567899','6845118321','ahmet@gmail.com','2','','','1','2','8618426545','64651351112','1','','','','','','')
+insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Selen','Ak','09.12.2000','05.07.2020','','Kentkonutlar','Kocaeli','KaramÃ¼rsel','41','45674125745','6845131321','selen@gmail.com','3','','','0','3','7624537532','46846512312','1','','','','','','')
+insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Buse','YÄ±lmaz','12.12.1999','03.12.2020','','Ferah Mah. Ã‡amlÄ±ca','Ä°stanbul','ÃœskÃ¼dar','34','78945612332','6851318321','buse@gmail.com','4','','','0','4','7867343455','23453786544','1','','','','','','')
+insert into tbl_Personeller(Pers_Adi,Pers_Soyadi,Pers_DTarihi,Pers_Ise_Giris_Tarihi,Pers_Isten_Cikis_Tarihi,Pers_Adresi,Pers_Ili,Pers_Kenti,Pers_Il_Kodu,Pers_Tel,Pers_Cep,Pers_Email,Bolum_ID,Pers_Maas,Pers_Komisyon_Yuzdesi,Cinsiyet_ID,Unvan_ID,Pers_SGK_No,Pers_TC_No,Pers_Aktif_Mi,Pers_Foto,Pers_CV,Pers_CV_File,Pers_CV_Web,Kaydeden,Kayit_Tarihi) values('Aleyna','Ã–z','11.05.2000','04.02.2020','','DumlupÃ½nar Mah. 19/05','Trabzon','Of','61','12312312345','68451318321','aleyna@gmail.com','5','','','0','5','4264146748','45126435778','1','','','','','','')
 
 
-insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('K','VT Yönetimi','Karamürsel','41','Kocaeli','Türkiye','Ocak','Bilişim')
-insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('E','Satış Elemanı','Karamürsel','41','Kocaeli','Türkiye','Mart','Kentleşme')
-insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('K','Pazarlamacı','Karamürsel','41','Kocaeli','Türkiye','Ekim','Bilim')
-insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('K','Ofis Yöneticisi','Karamürsel','41','Kocaeli','Türkiye','Aralık','Öğretim')
-insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('K','CIO','Derince','41','Kocaeli','Türkiye','Mayıs','Yönetim')
+insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('K','VT YÃ¶netimi','KaramÃ¼rsel','41','Kocaeli','TÃ¼rkiye','Ocak','BiliÅŸim')
+insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('E','SatÄ±ÅŸ ElemanÄ±','KaramÃ¼rsel','41','Kocaeli','TÃ¼rkiye','Mart','KentleÅŸme')
+insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('K','PazarlamacÄ±','KaramÃ¼rsel','41','Kocaeli','TÃ¼rkiye','Ekim','Bilim')
+insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('K','Ofis YÃ¶neticisi','KaramÃ¼rsel','41','Kocaeli','TÃ¼rkiye','AralÄ±k','Ã–ÄŸretim')
+insert into tbl_Kategoriler(Cinsiyet,Unvan,Kent_Adi,Il_Kodu,Il_Adi,Ulke,Ay_Adi,Kitap_Turu) values ('K','CIO','Derince','41','Kocaeli','TÃ¼rkiye','MayÄ±s','YÃ¶netim')
 
 
 insert into tbl_PersonelMaaslari(Pers_ID,Maas_Odeme_Tarihi,Maas_Tutari,Maas_Komisyon,Ay_ID,Maas_Yili)values ('1','01.1.2020','5000','600','1','2020')
@@ -124,7 +124,7 @@ insert into tbl_PersonelMaaslari(Pers_ID,Maas_Odeme_Tarihi,Maas_Tutari,Maas_Komi
 
 
 
-/****** ALTER TABLE ile FOREIGN KEYLERİ OLUŞTURMA ******/
+/****** ALTER TABLE ile FOREIGN KEYLERÄ° OLUÅTURMA ******/
 
 
 ALTER TABLE tbl_Personeller ADD FOREIGN KEY (Bolum_ID) REFERENCES tbl_Bolumler (Bolum_ID)
@@ -135,7 +135,7 @@ ALTER TABLE tbl_PersonelMaaslari ADD FOREIGN KEY (Ay_ID) REFERENCES tbl_Kategori
 
 
 
-/****** Tabloları Sorgulama ******/
+/****** TablolarÄ± Sorgulama ******/
 
 
 SELECT * FROM tbl_Bolumler
